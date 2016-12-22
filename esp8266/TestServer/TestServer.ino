46d182856517316529afc08028605a9563fd228b
duncan law <mrdunk@gmail.com>
2016-12-22 01:17:18 +0000

    Add full MQTT addressing to esp8266 firmware.

--- esp8266/TestServer/TestServer.ino
+++ esp8266/TestServer/TestServer.ino
@@ -21,6 +21,11 @@
 // Length of name strings. (hostname, room names, lamp names, etc.)
 #define NAME_LEN 32
 
+// Each device has an address in the form 'role/location1/location2/etc'
+// eg: 'lighting/kitchen/worktop/left'.
+#define ADDRESS_SEGMENTS 6
+#define ADDRESS_SEGMENT_LEN 32
+
 
 enum Io_Type {
   test,
@@ -29,9 +34,12 @@ enum Io_Type {
   onoff
 };
 
+typedef struct Address_Segment {
+  char segment[ADDRESS_SEGMENT_LEN];
+} Address_Segment;
+
 typedef struct Connected_device {
-  char name[NAME_LEN];
-  char room[NAME_LEN];
+  Address_Segment address_segment[ADDRESS_SEGMENTS];
   Io_Type io_type;
   int io_pins[3];
   int io_value[3];
@@ -50,18 +58,33 @@ struct Config {
 
 String mac_address;
 const int led = 12;
-const int led2 = 13;
 
 
 // Configuration
 void SetHostname(const char* new_hostname) {
-  strncpy(config.hostname, new_hostname, NAME_LEN);
+  strncpy(config.hostname, new_hostname, NAME_LEN -1);
+  config.hostname[NAME_LEN] = '\0';
 }
 
-void SetDevice(const unsigned int index, struct Connected_device device) {
+void SetDevice(const unsigned int index, struct Connected_device& device) {
   if (index < MAX_DEVICES) {
     memcpy(&(config.devices[index]), &device, sizeof(device));
   }
+}
+
+String DeviceAddress(Connected_device& device) {
+  String return_value = "";
+  for(int i = 0; i < ADDRESS_SEGMENTS; i++){
+    if(strlen(device.address_segment[i].segment) > 0){
+      if(i > 0){
+        return_value += "/";
+      }
+      return_value += device.address_segment[i].segment;
+    } else {
+      break;
+    }
+  }
+  return return_value;
 }
 
 
@@ -72,8 +95,6 @@ void answerCallback(const mdns::Answer* answer) {
   brokers.ParseMDnsAnswer(answer);
 }
 mdns::MDns my_mdns(NULL, NULL, answerCallback);
-
-
 
 
 // MQTT
@@ -93,11 +114,11 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
 
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
-    digitalWrite(led2, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    digitalWrite(led, LOW);   // Turn the LED on (Note that LOW is the voltage level
     // but actually the LED is on; this is because
     // it is acive low on the ESP-01)
   } else {
-    digitalWrite(led2, HIGH);  // Turn the LED off by making the voltage HIGH
+    digitalWrite(led, HIGH);  // Turn the LED off by making the voltage HIGH
   }
 }
 
@@ -107,15 +128,35 @@ void mqtt_connect() {
   mqtt_client.setCallback(mqtt_callback);
   if (mqtt_client.connect(config.hostname)) {
     char lamp[255];
-    strncpy(lamp, "_subject : lighting/unconfigured/", 254);
-    strncat(lamp, config.hostname, 254 - strlen(lamp));
-    strncat(lamp, " , _state : off", 254 - strlen(lamp));
+    char address[255];
+    
+    strncpy(address, "homeautomation/+/_all/_all", 254);
+    mqtt_client.subscribe(address);
 
-    mqtt_client.publish("homeautomation/0/lighting/_announce", lamp);
-    //mqtt_client.subscribe("homeautomation/+/lighting/unconfigured/" + config.hostname);
-    mqtt_client.subscribe("homeautomation/+/lighting/unconfigured/_all");
-    mqtt_client.subscribe("homeautomation/+/lighting/_all");
-    mqtt_client.subscribe("homeautomation/+/_all/_all");
+    for (int i = 0; i < MAX_DEVICES; ++i) {
+      if (strlen(config.devices[i].address_segment[0].segment) > 0) {
+        String announce = "_subject : " + DeviceAddress(config.devices[i]);
+        announce += " , _state : off";
+        announce.toCharArray(lamp, 255);
+
+        mqtt_client.publish("homeautomation/0/lighting/_announce", lamp);
+        
+        strncpy(address, "homeautomation/+/_all", 254);
+
+        for(int j = 0; j < ADDRESS_SEGMENTS; j++){
+          if(strlen(config.devices[i].address_segment[j].segment) <= 0) {
+            break;
+          }
+          address[strlen(address) -4] = '\0';
+          strncat(address, config.devices[i].address_segment[j].segment, 254 - strlen(address));
+          if (j < (ADDRESS_SEGMENTS -1) && strlen(config.devices[i].address_segment[j +1].segment) > 0) {
+            strncat(address, "/_all", 254 - strlen(address));
+          }
+          Serial.println(address);
+          mqtt_client.subscribe(address);
+        }
+      }
+    }
   }
   brokers.RateBroker(mqtt_client.connected());
   if (mqtt_client.connected()) {
@@ -162,12 +203,10 @@ void handleRoot() {
   message += "\n";
 
   for (int i = 0; i < MAX_DEVICES; ++i) {
-    if (strlen(config.devices[i].name)) {
-      message += i;
+    message += i;
+    if (strlen(config.devices[i].address_segment[0].segment) > 0) {
       message += "  ";
-      message += config.devices[i].name;
-      message += "  ";
-      message += config.devices[i].room;
+      message += DeviceAddress(config.devices[i]);
       message += "  ";
       if (config.devices[i].io_type == Io_Type::test) {
         message += "test";
@@ -188,9 +227,8 @@ void handleRoot() {
           message += config.devices[i].io_pins[0];
         }
       }
-      
-      message += "\n";
     }
+    message += "\n";
   }
 
   message += brokers.Summary();
@@ -200,25 +238,45 @@ void handleRoot() {
 }
 
 void handleConfig() {
+  Serial.println("handleConfig()");
   const unsigned int now = millis() / 1000;
 
   String message = "";
 
+  for(int i = 0; i < http_server.args(); i++){
+    message += http_server.argName(i);
+    message += '\t';
+    message += http_server.arg(i);
+    message += '\n';
+  }
+  message += '\n';
+
   if (http_server.hasArg("test_arg")) {
     message += "test_arg: " + http_server.arg("test_arg") + "\n";
   }
+
   if (http_server.hasArg("hostname")) {
     char tmp_buffer[NAME_LEN];
     http_server.arg("hostname").toCharArray(tmp_buffer, NAME_LEN);
     SetHostname(tmp_buffer);
     message += "hostname: " + http_server.arg("hostname") + "\n";
   }
-  Serial.println("device");
-  if (http_server.hasArg("device") and http_server.hasArg("name") and http_server.hasArg("room") and http_server.hasArg("io_type") and http_server.hasArg("io_pins")) {
+
+  if (http_server.hasArg("device") and http_server.hasArg("address_segment") and
+      http_server.hasArg("io_type") and http_server.hasArg("io_pins")) {
     unsigned int index = http_server.arg("device").toInt();
     Connected_device device;
-    http_server.arg("name").toCharArray(device.name, NAME_LEN);
-    http_server.arg("room").toCharArray(device.room, NAME_LEN);
+
+    int segment_counter = 0;
+    for(int i = 0; i < http_server.args(); i++){
+      if(http_server.argName(i) == "address_segment"){
+        http_server.arg(i).toCharArray(device.address_segment[segment_counter++].segment, ADDRESS_SEGMENT_LEN);
+      }
+    }
+    for(int i = segment_counter; i < ADDRESS_SEGMENTS; i++){
+      device.address_segment[segment_counter++].segment[0] = '\0';
+    }
+
     if (http_server.arg("io_type") == "test") {
       device.io_type = Io_Type::test;
     } else if (http_server.arg("io_type") == "rgb") {
@@ -238,14 +296,10 @@ void handleConfig() {
       io_pins_buffer[strlen(io_pins_buffer)] = ',';
     }
 
-    Serial.println(io_pins_buffer);
-    Serial.println();
-
     unsigned int pin_num = 0, pin_buffer_pointer = 0;
     memset(pin_buffer, '\0', 4);
     for (unsigned int i = 0; i < strlen(io_pins_buffer); ++i) {
       if (io_pins_buffer[i] == ',') {
-        Serial.println(pin_buffer);
         device.io_pins[pin_num++] = atoi(pin_buffer);
         memset(pin_buffer, '\0', 4);
         pin_buffer_pointer = 0;
@@ -257,12 +311,14 @@ void handleConfig() {
         break;
       }
     }
-    Serial.println();
 
     SetDevice(index, device);
 
     message += "device: " + http_server.arg("device") + "\n";
     Serial.println(message);
+
+    // Force reconnect to MQTT so we subscribe to any new addreses.
+    mqtt_client.disconnect();
   }
 
   http_server.send(200, "text/plain", message);
